52b712a4cd99154649cb1e82ad7305cda4401a72
Josh Bowden <git@josh.tf>
2018-09-21 14:59:34 +1000

    added build script for auto update of dev server - misc cleanup

---
+++ scripts/deploy.php
@@ -0,0 +1,72 @@
+<?php
+
+$inputSecret = htmlspecialchars($_GET["secret"]);
+$serverSecret = $_SERVER['SYNC_SECRET'];
+
+if($inputSecret != $serverSecret){
+    die("Unauthorised access - please provide the secret");
+}
+
+# set our working directory
+chdir('/git-dir');
+
+$exec = array(
+    'cd /git-dir',
+    'echo $PWD',
+    'whoami',
+    'git reset --hard HEAD',
+    'git pull',
+    'git status',
+    'git submodule sync',
+    'git submodule update',
+    'git submodule status',
+);
+
+$output = null;
+foreach ($exec as $cmd) {
+    $result = shell_exec($cmd);
+    $term .= "<span style=\"color: #ff3333;\">\$</span> <span style=\"color: #ff8686;\">{$cmd}\n</span>";
+    $term .= htmlentities(trim($result)) . "\n";
+}
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Github Sync Script [josh-tf]</title>
+</head>
+
+<body style="background: #333; color: #FFFFFF;">
+    <pre>
+
+##############################
+#                            #
+#  Github Sync Script v1.0   #
+#    github.com/josh-tf      #
+#                            #
+##############################
+
+<?php
+
+  echo $term;
+
+$webhook = $_SERVER['SLACK_WEBHOOK'];
+
+  define('SLACK_WEBHOOK', $webhook);
+  $message = array('payload' => json_encode(array('text' => 'A rebuild has successfully been completed on the Development build server')));
+  $c = curl_init(SLACK_WEBHOOK);
+  curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($c, CURLOPT_POST, true);
+  curl_setopt($c, CURLOPT_POSTFIELDS, $message);
+  curl_exec($c);
+  curl_close($c);
+
+
+ ?>
+
+</pre>
+</body>
+
+</html>
