b28b7f58821da0fbee4510d147d7ccded19eb779
Josh Bowden <git@josh.tf>
2018-09-21 16:56:18 +1000

    fixed issue with php string as autoformat was adding an extra space

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -131,8 +131,8 @@
 					<ul class="nav navbar-nav navbar-right">
 						<?php foreach ($allowed_modules as $module): ?>
 						<li class="<?php echo $module->module_id == $this->uri->segment(1) ? 'active' : ''; ?>">
-							<a href="<?php echo site_url(" $module->module_id"); ?>" title="
-								<?php echo $this->lang->line("module_" . $module->module_id); ?>" class="menu-icon">
+							<a href="<?php echo base_url() . $module->module_id; ?>" title="
+								<?php echo $this->lang->line(" module_" . $module->module_id); ?>" class="menu-icon">
 								<img src="<?php echo base_url() . 'images/menubar/' . $module->module_id . '.png'; ?>" border="0" alt="Module Icon" /><br />
 								<?php echo $this->lang->line("module_" . $module->module_id) ?>
 							</a>
