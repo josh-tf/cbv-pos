2b15f7eae1136b06edb95f9f182c6a832702f427
Josh Bowden <git@josh.tf>
2018-09-27 11:31:20 +1000

    working on mysql import solution

--- scripts/update-db.sh
+++ scripts/update-db.sh
@@ -2,20 +2,19 @@
 
 # joshtf/cbvpos
 # This script is to update the db on the dev server, it is called manually
-
 start=$SECONDS
 
 echo "Downloading latest db import from github"
-rm cbvpos_import.sql
+rm -f cbvpos_import.sql
 
 wget -q https://raw.githubusercontent.com/josh-tf/cbvpos/master/database/cbvpos_import.sql
 
 echo "Deleting old data and creating new db"
-mysqladmin -u$MYSQL_USER -p$MYSQL_PASSWORD DROP cbvpos -f;
-mysqladmin -u$MYSQL_USER -p$MYSQL_PASSWORD CREATE cbvpos;
+mysqladmin -h $2 -u$3 -p$4 DROP $1 -f;
+mysqladmin -h $2 -u$3 -p$4 CREATE $1;
 
 echo "Importing latest db import in to cbvpos db"
-mysql -u$MYSQL_USER -p$MYSQL_PASSWORD cbvpos < cbvpos_import.sql
+mysql -h $2 -u$3 -p$4 cbvpos < cbvpos_import.sql
 
 timer=$(( SECONDS - start ))
 echo "Done.. ($timer seconds)"
