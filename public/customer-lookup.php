a265c147c3b31d69de1db79c418c825f5336d636
Josh Bowden <git@josh.tf>
2018-09-29 13:10:39 +1000

    added date range to conc lookup tool

--- public/customer-lookup.php
+++ public/customer-lookup.php
@@ -6,11 +6,13 @@ $username = getenv('MYSQL_USERNAME');
 $password = getenv('MYSQL_PASSWORD');
 $dbname = getenv('MYSQL_DB_NAME');
 
-$concID = htmlspecialchars($_GET["conc-id"]);
+$concID = htmlspecialchars($_GET['conc-id']);
 
 if(!$concID){
   die('Invalid ID provided');
 }
+
+$saleRange = ucfirst(htmlspecialchars($_GET['range']));
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -43,6 +45,7 @@ $matchCount = mysqli_num_rows($result);
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
 
   <link rel="stylesheet" href="css/extra/normalize.css">
   <link rel="stylesheet" href="css/extra/skeleton.css">
@@ -123,6 +126,29 @@ while ($row = $result->fetch_assoc()) {
 
 <?php
 
+$searchRange = new DateTime();
+
+switch ($saleRange) {
+  case 'Week':
+          $searchRange->modify('-1 week');
+      break;
+  case 'Month':
+          $searchRange->modify('-1 month');
+      break;
+  case 'Year':
+          $searchRange->modify('-1 year');
+      break;
+      default:
+      $searchRange->modify('-10 years');
+      $saleRange = 'All Time';
+}
+
+if($saleRange == 'All Time'){
+    $timeString = $saleRange;
+  }else{
+    $timeString = $saleRange . ' (from ' . $searchRange->format('d m Y') . ')';
+  }
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 $sql = "SELECT *";
@@ -137,7 +163,7 @@ $sql .= "     cbvpos_items t4 ON t3.item_id = t4.item_id";
 $sql .= "         INNER JOIN";
 $sql .= "     cbvpos_people t5 ON t2.person_id = t5.person_id";
 $sql .= " WHERE";
-$sql .= " 	company_name = '" . $concID .  "';";
+$sql .= " 	company_name = '" . $concID .  "' AND sale_time >= '" . $searchRange->format('Y-m-d H:i:s') . "';";
 
 $stmt = $conn->prepare($sql);
 
@@ -168,8 +194,49 @@ while ($num = mysqli_fetch_assoc($discountCount)) {
 
 <br><br>
 
-<h5>Sales for Concession ID [<b><?php echo $concID ?></b>] - Total Spent: <b>$<?php echo number_format((float) ($totalSpent-$discTotal), 2, '.', ''); ?></b> - Total Discount: <b>$<?php echo number_format((float) ($discTotal), 2, '.', ''); ?></b></h5>
+<h5>Sales for Concession ID [<b><?php echo $concID ?></b>] - <?php echo $timeString ?></h5>
 <p>There are <?php echo $matchCount; ?> matches for this concession ID in the sales database.</p>
+
+<table>
+<thead>
+<tr>
+<th>Description</th>
+<th>Total Amount</th>
+<th>Time Period</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Total Spent</td>
+<td>$<?php echo number_format((float) ($totalSpent-$discTotal), 2, '.', ''); ?></td>
+<td><?php echo $saleRange ?></td>
+</tr>
+<tr>
+<td>Total Discount</td>
+<td>$<?php echo number_format((float) ($discTotal), 2, '.', ''); ?></td>
+<td><?php echo $saleRange ?></td>
+</tr>
+
+</tbody>
+</table>
+
+<?php
+$siteurl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$siteurl = strstr($siteurl, '&', true);
+?>
+
+</table>
+<table>
+<tbody>
+<tr>
+<td><b>Search Period</b></td>
+<td><a href="<?php echo $siteurl . '&range=week'; ?>" class="button-<?php echo ($saleRange == 'Week') ? 'secondary' : 'warning'; ?> pure-button">Week</a></td>
+<td><a href="<?php echo $siteurl . '&range=month'; ?>" class="button-<?php echo ($saleRange == 'Month') ? 'secondary' : 'warning'; ?>  pure-button">Month</a></td>
+<td><a href="<?php echo $siteurl . '&range=year'; ?>" class="button-<?php echo ($saleRange == 'Year') ? 'secondary' : 'warning'; ?>  pure-button">Year</a></td>
+<td><a href="<?php echo $siteurl . '&range=alltime'; ?>" class="button-<?php echo ($saleRange == 'All Time') ? 'secondary' : 'warning'; ?>  pure-button">All Time</a></td>
+</tr>
+</tbody>
+</table>
 
 <table class="u-full-width">
 <thead>
