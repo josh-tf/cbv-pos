c0ec4d20ae01add81f645703079d2940b42a3c6d
Josh Bowden <git@josh.tf>
2018-10-19 12:07:56 +1100

    removing unused payment methods

--- application/models/Sale.php
+++ application/models/Sale.php
@@ -987,23 +987,23 @@ class Sale extends CI_Model
 		if($this->config->item('payment_options_order') == 'debitcreditcash')
 		{
 			$payments[$this->lang->line('sales_debit')] = $this->lang->line('sales_debit');
-			$payments[$this->lang->line('sales_credit')] = $this->lang->line('sales_credit');
+			//$payments[$this->lang->line('sales_credit')] = $this->lang->line('sales_credit');
 			$payments[$this->lang->line('sales_cash')] = $this->lang->line('sales_cash');
 		}
 		elseif($this->config->item('payment_options_order') == 'debitcashcredit')
 		{
 			$payments[$this->lang->line('sales_debit')] = $this->lang->line('sales_debit');
 			$payments[$this->lang->line('sales_cash')] = $this->lang->line('sales_cash');
-			$payments[$this->lang->line('sales_credit')] = $this->lang->line('sales_credit');
+			//$payments[$this->lang->line('sales_credit')] = $this->lang->line('sales_credit');
 		}
 		else // default: if($this->config->item('payment_options_order') == 'cashdebitcredit')
 		{
 			$payments[$this->lang->line('sales_cash')] = $this->lang->line('sales_cash');
 			$payments[$this->lang->line('sales_debit')] = $this->lang->line('sales_debit');
-			$payments[$this->lang->line('sales_credit')] = $this->lang->line('sales_credit');
+			//$payments[$this->lang->line('sales_credit')] = $this->lang->line('sales_credit');
 		}
 
-		$payments[$this->lang->line('sales_due')] = $this->lang->line('sales_due');
+		//$payments[$this->lang->line('sales_due')] = $this->lang->line('sales_due');
 		$payments[$this->lang->line('sales_check')] = $this->lang->line('sales_check');
 
 		if($giftcard)
