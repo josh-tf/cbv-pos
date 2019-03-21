53662d53607f660de99ef49e2d9aa682291f232a
Josh Bowden <git@josh.tf>
2019-03-22 08:58:21 +1100

    cleanup of formatting in two files

--- application/views/items/sales_ticket.php
+++ application/views/items/sales_ticket.php
@@ -42,193 +42,218 @@ foreach ($cbv_info as $computer) {
 ?>
 
 <style>
-.wrapper {
-  font-size: 16px;
-}
-    @media print {
-        @page {
-            size: landscape
-        }
-        .container {
-     width:90% !important;
-}
-div#content {
-    margin-top: 70px;
-}
+  .wrapper {
+    font-size: 16px;
+  }
+
+  @media print {
+    @page {
+      size: landscape
     }
 
-@page{
-margin-left: 0px;
-margin-right: 0px;
-margin-top: 0px;
-margin-bottom: 0px;
-}
+    .container {
+      width: 90% !important;
+    }
+
+    div#content {
+      margin-top: 70px;
+    }
+  }
+
+  @page {
+    margin-left: 0px;
+    margin-right: 0px;
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
 </style>
 
 <script type="text/javascript">
-    function printdoc() {
-        $('#printWarning').modal('hide');
-        window.print();
-    }
-
-
+  function printdoc() {
+    $('#printWarning').modal('hide');
+    window.print();
+  }
 </script>
 
 <!-- Modal -->
 <div class="modal" id="printWarning" tabindex="-1" role="dialog" aria-labelledby="printWarningLabel" aria-hidden="true">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="printWarningLabel">Printer Settings</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-<h3>Important</h3>
-<p>
-To ensure the sales ticket is printed correctly, please ensure
-the printer settings in the popup dialog are configured as follows:
-</p>
-<br>
-<table class="table table-bordered">
-  <thead>
-  <tr>
-    <th scope="col">Setting</th>
-    <th scope="col">Option</th>
-  </tr>
-  </thead>
-  <tbody>
-  <tr>
-    <th scope="row">2-Sided</th>
-    <td>Any</td>
-  </tr>
-  <tr>
-    <th scope="row">Orientation</th>
-    <td>Landscape</td>
-  </tr>
-  <tr>
-    <th scope="row">Scaling</th>
-    <td colspan="2">100%</td>
-  </tr>
-  <tr>
-    <th scope="row">Headers/Footers</th>
-    <td colspan="2">Off</td>
-  </tr>
-  </tbody>
-</table>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-					<button type="button" onclick="printdoc();" class="btn btn-primary">Print Sales Ticket</button>
-			</div>
-		</div>
-	</div>
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="printWarningLabel">Printer Settings</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h3>Important</h3>
+        <p>
+          To ensure the sales ticket is printed correctly, please ensure
+          the printer settings in the popup dialog are configured as follows:
+        </p>
+        <br>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Setting</th>
+              <th scope="col">Option</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">2-Sided</th>
+              <td>Any</td>
+            </tr>
+            <tr>
+              <th scope="row">Orientation</th>
+              <td>Landscape</td>
+            </tr>
+            <tr>
+              <th scope="row">Scaling</th>
+              <td colspan="2">100%</td>
+            </tr>
+            <tr>
+              <th scope="row">Headers/Footers</th>
+              <td colspan="2">Off</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" onclick="printdoc();" class="btn btn-primary">Print Sales Ticket</button>
+      </div>
+    </div>
+  </div>
 </div>
 
 <div id="title_bar" class="btn-toolbar print_hide">
 
-<button data-toggle="modal" data-target="#printWarning" class="btn btn-info btn-sm pull-right">
+  <button data-toggle="modal" data-target="#printWarning" class="btn btn-info btn-sm pull-right">
     <span class="glyphicon glyphicon-print">&nbsp</span>Print Ticket
-</button>
+  </button>
 
-<button class='btn btn-info btn-sm pull-right' onclick="window.location.href='/items/'">
+  <button class='btn btn-info btn-sm pull-right' onclick="window.location.href='/items/'">
     <span class="glyphicon glyphicon-tag">&nbsp</span>Back to Items
-</button>
+  </button>
 </div>
 
 
-    <div class="row" id="content">
-        <div class="col-sm-6">
+<div class="row" id="content">
+  <div class="col-sm-6">
 
-	  <div style="height:77px">
+    <div style="height:77px">
       <img src="images/ticket-logo.png" width="252px" style="float:left;" />
-      <div class="ticket-cbvid"><b>ID:</b> <?php echo $specID ?></div>
+      <div class="ticket-cbvid"><b>ID:</b>
+        <?php echo $specID ?>
+      </div>
     </div>
 
-<br />
+    <br />
 
-        <p class="summary">This computer includes a range of free software, a keyboard, mouse and monitor. All computers sold in store come with a 3 month back to base warranty.</p>
+    <p class="summary">This computer includes a range of free software, a keyboard, mouse and monitor. All computers
+      sold in store come with a 3 month back to base warranty.</p>
 
-<table class="table table-sm table-bordered table-striped">
-  <thead>
-    <tr>
-      <th>Price</th>
-      <th>Concession</th>
-      <th>Full</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td><b class="pricing">Full System</b></td> <!-- round up or down to nearest $5 -->
-      <td><b class="pricing"><?php echo $concPriceFull ?></b></td>
-      <td><?php echo $nonConcPriceFull ?></td></b></td><!-- the php below adds 25% and rounds up/down to nearest $5 -->
-    </tr>
-    <tr>
-      <td><b class="pricing">Box Only</b></td>
-      <td><b class="pricing"><?php echo $concPriceBox ?></b></td>
-      <td><?php echo $nonConcPriceBox ?></b></td>
-    </tr>
-  </tbody>
-</table>
+    <table class="table table-sm table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>Price</th>
+          <th>Concession</th>
+          <th>Full</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><b class="pricing">Full System</b></td> <!-- round up or down to nearest $5 -->
+          <td><b class="pricing">
+              <?php echo $concPriceFull ?></b></td>
+          <td>
+            <?php echo $nonConcPriceFull ?>
+          </td></b></td><!-- the php below adds 25% and rounds up/down to nearest $5 -->
+        </tr>
+        <tr>
+          <td><b class="pricing">Box Only</b></td>
+          <td><b class="pricing">
+              <?php echo $concPriceBox ?></b></td>
+          <td>
+            <?php echo $nonConcPriceBox ?></b></td>
+        </tr>
+      </tbody>
+    </table>
 
-<p class="discount-info">The Box Only price applies if you wish to purchase the desktop only, you will need to supply your own monitor, keyboard and mouse.</p>
+    <p class="discount-info">The Box Only price applies if you wish to purchase the desktop only, you will need to
+      supply your own monitor, keyboard and mouse.</p>
 
-<table class="table table-sm table-bordered table-striped ticket-specs">
-  <thead>
-    <tr>
-      <th>Spec</th>
-      <th>Details</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td><img src="images/ticket-icons/cbvid.png" class="ticket-icon" /> <b>Model</b></td>
-      <td><?php echo $specModel; ?></td>
-    </tr>
-    <tr>
-      <td><img src="images/ticket-icons/processor.png" class="ticket-icon" /> <b>Processor</b></td>
-      <td><?php echo $specCPU; ?></td>
-    </tr>
-    <tr>
-      <td><img src="images/ticket-icons/memory.png" class="ticket-icon" /> <b>Memory</b></td>
-      <td><?php echo $specRAM; ?></td>
-    </tr>
-    <tr>
-      <td><img src="images/ticket-icons/storage.png" class="ticket-icon" /> <b>Storage</b></td>
-      <td><?php echo $specHDD; ?></td>
-    </tr>
-    </tr>
-      <td><img src="images/ticket-icons/monitor.png" class="ticket-icon" /> <b>Monitor</b></td>
-      <td><?php echo $specMonitor ?></td>
-    </tr>
-    <tr>
-      <td><img src="images/ticket-icons/extras.png" class="ticket-icon" /> <b>Extras</b></td>
-      <td><?php echo $specEX ? $specEX : "None"; ?></td>
-  </tbody>
-</table>
+    <table class="table table-sm table-bordered table-striped ticket-specs">
+      <thead>
+        <tr>
+          <th>Spec</th>
+          <th>Details</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><img src="images/ticket-icons/cbvid.png" class="ticket-icon" /> <b>Model</b></td>
+          <td>
+            <?php echo $specModel; ?>
+          </td>
+        </tr>
+        <tr>
+          <td><img src="images/ticket-icons/processor.png" class="ticket-icon" /> <b>Processor</b></td>
+          <td>
+            <?php echo $specCPU; ?>
+          </td>
+        </tr>
+        <tr>
+          <td><img src="images/ticket-icons/memory.png" class="ticket-icon" /> <b>Memory</b></td>
+          <td>
+            <?php echo $specRAM; ?>
+          </td>
+        </tr>
+        <tr>
+          <td><img src="images/ticket-icons/storage.png" class="ticket-icon" /> <b>Storage</b></td>
+          <td>
+            <?php echo $specHDD; ?>
+          </td>
+        </tr>
+        </tr>
+        <td><img src="images/ticket-icons/monitor.png" class="ticket-icon" /> <b>Monitor</b></td>
+        <td>
+          <?php echo $specMonitor ?>
+        </td>
+        </tr>
+        <tr>
+          <td><img src="images/ticket-icons/extras.png" class="ticket-icon" /> <b>Extras</b></td>
+          <td>
+            <?php echo $specEX ? $specEX : "None"; ?>
+          </td>
+      </tbody>
+    </table>
 
-</div>
-        <div class="col-sm-6">
+  </div>
+  <div class="col-sm-6">
 
-	   <img src="images/ticket-linux.png" width="570px" />
+    <img src="images/ticket-linux.png" width="570px" />
 
-	  <h3>What is Linux?</h3>
-    <p>Our computers <b>do not</b> come with Microsoft Windows. We run Linux instead. Linux is similar to other systems you may have used before, such as Windows or OS X.</p>
-		<p>Our operating system includes a built in firewall and does not require any aditional antivirus software.
-		</p>
+    <h3>What is Linux?</h3>
+    <p>Our computers <b>do not</b> come with Microsoft Windows. We run Linux instead. Linux is similar to other systems
+      you may have used before, such as Windows or OS X.</p>
+    <p>Our operating system includes a built in firewall and does not require any aditional antivirus software.
+    </p>
 
-	  <h3>What Software is included?</h3>
-        <p>Every Computerbank computer comes bundled with a range of useful software including Chrome and Firefox, the OpenOffice office suite and a range of games and other useful tools.</p>
-        <p>Please ask us if you have any questions about the included or available software.</p>
+    <h3>What Software is included?</h3>
+    <p>Every Computerbank computer comes bundled with a range of useful software including Chrome and Firefox, the
+      OpenOffice office suite and a range of games and other useful tools.</p>
+    <p>Please ask us if you have any questions about the included or available software.</p>
 
-	  <h3>Warranty Information</h3>
-		Our restored computers come with a <b>three months</b> back to base warranty.</p>
-    <p>For desktops, we extend the warranty to <b>six months</b> if the original operating system, Linux, is still installed on the computer.</p>
+    <h3>Warranty Information</h3>
+    Our restored computers come with a <b>three months</b> back to base warranty.</p>
+    <p>For desktops, we extend the warranty to <b>six months</b> if the original operating system, Linux, is still
+      installed on the computer.</p>
     <p>If you have a problem you can organise an appointment to bring it back in.</p>
-		<br />
+    <br />
 
+  </div>
 </div>
-    </div>
 
 <?php $this->load->view("partial/footer");?>
