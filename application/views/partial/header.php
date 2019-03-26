19b013594897eb90410f9085611e54e6a5c0a96d
Josh Bowden <git@josh.tf>
2019-03-26 21:14:58 +1100

    fix the tooltips for the menubar items - Trello OPdQbPYc

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -130,8 +130,7 @@
 					<ul class="nav navbar-nav navbar-right">
 						<?php foreach ($allowed_modules as $module): ?>
 						<li class="<?php echo $module->module_id == $this->uri->segment(1) ? 'active' : ''; ?>">
-							<a href="<?php echo base_url() . $module->module_id; ?>" title="
-								<?php echo $this->lang->line(" module_" . $module->module_id); ?>" class="menu-icon">
+							<a href="<?php echo base_url() . $module->module_id; ?>" title="<?php echo $this->lang->line("module_".$module->module_id.'_desc') ?>" class="menu-icon">
 								<img src="<?php echo base_url() . 'images/menubar/' . $module->module_id . '.png'; ?>" border="0" alt="Module Icon" /><br />
 								<?php echo $this->lang->line("module_" . $module->module_id) ?>
 							</a>
