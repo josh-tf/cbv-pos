ba39a880a4ec12100483321c9310c5bf23977afa
Josh Bowden <git@josh.tf>
2019-04-12 20:08:54 +1000

    add lookup button to customer list - Trello: nZYsvJY2

--- app/application/views/items/redir.php
+++ app/application/views/items/redir.php
@@ -2,8 +2,8 @@
 echo form_open(base_url('items/sales_ticket'), array('id' => 'redir', 'class' => 'redir'));
 
 // intermediary script to handle the post reqeust
-$cbv_id = $this->input->get('id', true);
-echo '<input type="hidden" name="cbv_id" value="' . htmlentities($cbv_id) . '">';
+$cbvid_check = $this->input->get('id', true);
+echo '<input type="hidden" name="cbvid_check" value="' . htmlentities($cbvid_check) . '">';
 
 echo form_close();
 ?>
