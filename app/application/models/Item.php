48f3a7ff942e3e669b934b36ff38e7739a40cdda
Josh Bowden <git@josh.tf>
2019-04-14 01:28:11 +1000

    hide item from stocklist on_hold - Trello: h1hB3Quf

--- app/application/models/Item.php
+++ app/application/models/Item.php
@@ -954,6 +954,7 @@ class Item extends CI_Model
         $this->db->from('cbvpos_item_quantities');
         $this->db->join('cbvpos_items', 'cbvpos_items.item_id = cbvpos_item_quantities.item_id');
         $this->db->where('quantity > 0');
+        $this->db->where('on_hold', false); // hide on_hold items from stocklist
         $this->db->order_by('unit_price asc');
 
         // pass as the function result
