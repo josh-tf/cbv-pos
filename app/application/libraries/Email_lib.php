9a2513ccd7f3c469293ac20c49e23366695d6be8
Josh Bowden <git@josh.tf>
2019-04-17 21:07:39 +1000

    start solution for emailing - Trello: QDr2t8WS

--- app/application/libraries/Email_lib.php
+++ app/application/libraries/Email_lib.php
@@ -1,4 +1,6 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
 /**
  * Email library
@@ -8,57 +10,62 @@
 
 class Email_lib
 {
-	private $CI;
+    private $CI;
 
-  	public function __construct()
-	{
-		$this->CI =& get_instance();
+    public function __construct()
+    {
+        $this->CI = &get_instance();
 
-		$this->CI->load->library('email');
+        $this->CI->load->library('email');
 
-		$config = array(
-			'mailtype' => 'html',
-			'useragent' => 'OSPOS',
-			'validate' => TRUE,
-			'protocol' => $this->CI->config->item('protocol'),
-			'mailpath' => $this->CI->config->item('mailpath'),
-			'smtp_host' => $this->CI->config->item('smtp_host'),
-			'smtp_user' => $this->CI->config->item('smtp_user'),
-			'smtp_pass' => $this->CI->encryption->decrypt($this->CI->config->item('smtp_pass')),
-			'smtp_port' => $this->CI->config->item('smtp_port'),
-			'smtp_timeout' => $this->CI->config->item('smtp_timeout'),
-			'smtp_crypto' => $this->CI->config->item('smtp_crypto')
-		);
+        $config = array(
+            'mailtype' => 'html',
+            'useragent' => 'OSPOS',
+            'validate' => true,
+            'protocol' => $this->CI->config->item('protocol'),
+            'mailpath' => $this->CI->config->item('mailpath'),
+            'smtp_host' => $this->CI->config->item('smtp_host'),
+            'smtp_user' => $this->CI->config->item('smtp_user'),
+            'smtp_pass' => $this->CI->encryption->decrypt($this->CI->config->item('smtp_pass')),
+            'smtp_port' => $this->CI->config->item('smtp_port'),
+            'smtp_timeout' => $this->CI->config->item('smtp_timeout'),
+            'smtp_crypto' => $this->CI->config->item('smtp_crypto'),
+        );
 
-		$this->CI->email->initialize($config);
-	}
+        $this->CI->email->initialize($config);
+    }
 
-	/**
-	 * Email sending function
-	 * Example of use: $response = sendEmail('john@doe.com', 'Hello', 'This is a message', $filename);
-	 */
-	public function sendEmail($to, $subject, $message, $attachment = NULL)
-	{
-		$email = $this->CI->email;
+    /**
+     * Email sending function
+     * Example of use: $response = sendEmail('john@doe.com', 'Hello', 'This is a message', $filename);
+     */
+    public function sendEmail($to, $subject, $message, $attachment = null)
+    {
+        require 'class.smtp.php';
+        require 'class.phpmailer.php';
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->Debugoutput = 'html';
+        $mail->SMTPAuth = true;
+        $mail->Host = $this->CI->config->item('smtp_host');
+        $mail->Port = $this->CI->config->item('smtp_port');
+        $mail->addAddress($to);
+        if (!empty($attachment)) {
+            $mail->addStringAttachment($attachment);
+        }
+        $mail->Username = $this->CI->config->item('smtp_user');
+        $mail->Password = $this->CI->encryption->decrypt($this->CI->config->item('smtp_pass'));
+        $mail->setFrom($this->CI->config->item('smtp_user'), $this->config->item('company'));
+        $mail->addReplyTo($this->CI->config->item('smtp_user'), $this->config->item('company'));
+        $mail->Subject = $subject;
+        $mail->msgHTML($message);
+        $mail->send();
+        $result = $mail->send();
+        if (!$result) {
+            error_log($email->print_debugger());
+        }
+        return $result;
 
-		$email->from($this->CI->config->item('email'), $this->CI->config->item('company'));
-		$email->to($to);
-		$email->subject($subject);
-		$email->message($message);
-		if(!empty($attachment))
-		{
-			$email->attach($attachment);
-		}
-
-		$result = $email->send();
-
-		if(!$result)
-		{
-			error_log($email->print_debugger());
-		}
-
-		return $result;
-	}
+    }
 }
-
-?>
