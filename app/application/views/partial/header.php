16bc4ef6f92a9ec717932c07562685a4f02fa67b
Josh Bowden <git@josh.tf>
2019-05-26 17:05:50 +1000

    update styling and icon sets

--- app/application/views/partial/header.php
+++ app/application/views/partial/header.php
@@ -154,7 +154,7 @@
 						<?php foreach ($allowed_modules as $module): ?>
 						<li class="<?php echo $module->module_id == $this->uri->segment(1) ? 'active' : ''; ?>">
 							<a href="<?php echo base_url() . $module->module_id; ?>" title="<?php echo $this->lang->line("module_".$module->module_id.'_desc') ?>" class="menu-icon">
-								<img src="<?php echo base_url() . 'images/menubar/' . $module->module_id . '.png'; ?>" border="0" alt="Module Icon" /><br />
+								<img src="<?php echo base_url() . 'images/menubar/white/' . $module->module_id . '.png'; ?>" border="0" alt="Module Icon" /><br />
 								<?php echo $this->lang->line("module_" . $module->module_id) ?>
 							</a>
 						</li>
