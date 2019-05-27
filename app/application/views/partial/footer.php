3b976b398ae9da146cc5cf8527684215dc10e680
Josh Bowden <git@josh.tf>
2019-05-27 16:24:11 +1000

    update footer text per original authors requests - Trello: o7LrGR03

--- app/application/views/partial/footer.php
+++ app/application/views/partial/footer.php
@@ -3,7 +3,7 @@
 
 	<div id="footer">
 		<div class="jumbotron push-spaces">
-			<strong>CBV-POS version <?php echo $this->config->item('application_version'); ?></Strong> (<a target="_blank" href="https://github.com/josh-tf/cbv-pos">source</a></a>) - Forked from <a target="_blank" href="https://github.com/opensourcepos/opensourcepos">opensourcepos</a></a> project - icons by <a target="_blank"  href="https://icons8.com"/>icons8</a></strong>
+			<strong>CBV-POS version <?php echo $this->config->item('application_version'); ?></Strong> (<a target="_blank" href="https://github.com/josh-tf/cbv-pos">source</a></a>) - Forked from <a target="_blank" href="https://github.com/opensourcepos/opensourcepos">Open Source Point Of Sale</a></a> 3.2.0 (<?php echo substr('$Id: 4f5ad5761503bbc94152cbf16afa76439ca155b8 $', 5, 7); ?>) - icons by <a target="_blank"  href="https://icons8.com"/>icons8</a></strong>
 		</div>
 	</div>
 </body>
